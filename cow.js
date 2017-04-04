9f31ac6d2d7549b665ddd8026602a52d35918097
Owen Shepherd <414owen@gmail.com>
2017-04-04 15:07:20 +0100

    Major compatability update
    Also performed a lot of minification-enabling optimisations

--- cow.js
+++ cow.js
@@ -19,58 +19,61 @@
 
 	// Repeat a string
 	function rep(a, b) {
-		return Array(b).fill(a).join("");
+		var res = "";
+		while (b--) {res += a;}
+		return res;
 	}
 
-	// State and whitespace regex
-	var text = "", ws = /^\s*$/;
+	// State, whitespace regex, space alias
+	var text = "", ws = /^\s*$/, space = " ",
+		repSpace = rep.bind(space, space);
 
 	function cow(a) {
 
 		// Array of lines based on newlines and 50 char limit
-		var s = a.split("\n").reduce(function(a, s) {
+		var split = a.split("\n").reduce(function(a, s) {
 			var res = [];
 			while (s.length > 50) {
-				var t, n, f = "";
-				if (ws.test(s[49]))      {t = 49; n = 50;} 
-				else if (ws.test(s[50])) {t = 50; n = 51;} 
-				else if (ws.test(s[48])) {t = 48; n = 49;} 
-				else           {f = "-";  t = 49; n = 49;}
-				res.push(s.slice(0, t) + f);
-				s = s.slice(n);
+				var t = 49, n = 50, f = "";
+				if (ws.test(s[50])) {t++; n++;} 
+				else if (ws.test(s[48])) {t--; n--;} 
+				else if (!ws.test(s[49])) {f = "-"; n--;}
+				res.push((s.substr(0, t) + f).trim());
+				s = s.substr(n);
 			}
-			res.push(s);
+			res.push(s.trim());
 			return a.concat(res);
 		}, []);
-
 		// Trim blank lines, but leave spaces in for formatting
-		while (ws.test(s[0]))            {s = s.slice(1);}
-		while (ws.test(s[s.length - 1])) {s = s.slice(0, s.length - 1);}
+		while (ws.test(split[0])) {split = split.slice(1);}
+		while (ws.test(split[split.length - 1])) {split = split.slice(0, split.length - 1);}
 
 		// Don't print nothing
-		if (s.length === 0) {return;}
+		if (split.length === 0) {return;}
 
 		// Find longest line
-		var l = s.reduce(function(acc, val) {
+		var longest = split.reduce(function(acc, val) {
 			return acc > val.length ? acc : val.length;
 		}, 0);
 
 		// Generate text box sides
-		var sides = s.length === 1 ? "<>" : 
-			"/\\" + rep("|", s.length * 2 - 4) + "\\/";
+		var sides = split.length === 1 ? "<>" : 
+			"/\\" + rep("|", split.length * 2 - 4) + "\\/";
 
-		console.log(
-			" " + rep("_", l + 2) + " \n" +
-			s.map(function(b, i) {
-				return sides[i * 2] + " " + b + rep(" ", l - b.length) + 
-					" " + sides[i * 2 + 1] + "\n";
+		var cow = rep("_", longest + 2) + "\n" +
+			split.map(function(b, i) {
+				return sides[i * 2] + space + b + repSpace(longest - b.length) + 
+					space + sides[i * 2 + 1] + "\n";
 			}).join("") +
-			" " + rep("-", l + 2) + " \n" +
-			"        \\   ^__^\n" +
-			"         \\  (oo)\\_______\n" +
-			"            (__)\\       )\\/\\\n" +
-			"                ||----w |\n" +
-			"                ||     ||"
+			space + rep("-", longest + 2) + "\n" +
+			repSpace(8)  + "\\   ^__^\n" +
+			repSpace(9)  + "\\  (oo)\\_______\n" +
+			repSpace(12) + "(__)\\       )\\/\\\n" +
+			repSpace(16) + "||----w |\n" +
+			repSpace(16) + "||     ||";
+
+		console.log("%c " + cow,
+			"font-family: monospace"
 		);
 	}
 
@@ -87,9 +90,18 @@
 			if (e.shiftKey) {text += "\n";} 
 			else {cow(text); text  = "";}
 		} else if (e.keyCode === 8) {
-			text = text.slice(0, text.length - 1);
-		} else if (e.key.length === 1) {
+			text = text.substr(0, text.length - 1);
+		} else if (e.key && e.key.length === 1) {
 			text += e.key;
+		} else if (e.keyIdentifier) {
+			var code = parseInt(e.keyIdentifier.substr(2), 16);
+			if (code > 0) {
+				var key = String.fromCharCode(code);
+				if (!e.shiftKey) {key = key.toLowerCase();}
+				if (key.length === 1) {
+					text += key;
+				}
+			}
 		}
 	};
 
